370e4b54cd3f96cfda9a8e322974a9eaeb3995d6
elitecodegroovy <elite_jigang@163.com>
2019-07-19 15:46:35 +0800

    Added a middleware

--- apps/web/main.go
+++ apps/web/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"context"
+	"github.com/gin-gonic/gin/binding"
+	"gopkg.in/go-playground/validator.v8"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"reflect"
+	"syscall"
 	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -50,15 +58,119 @@ func SetDataD(c *gin.Context) {
 	})
 }
 
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//t := time.Now()
+
+		// Set `www.baidu.com` variable
+		c.Set("www.baidu.com", "192.168.1.229")
+		//
+		//// before request
+		//
+		//c.Next()
+		//
+		//// after request
+		//latency := time.Since(t)
+		//log.Print(latency)
+		//
+		//// access the status we are sending
+		//status := c.Writer.Status()
+		//log.Println(status)
+	}
+}
+
+func setR(r *gin.Engine) {
+	//r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+	//	// your custom format
+	//	return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+	//		param.ClientIP,
+	//		param.TimeStamp.Format(time.RFC1123),
+	//		param.Method,
+	//		param.Path,
+	//		param.Request.Proto,
+	//		param.StatusCode,
+	//		param.Latency,
+	//		param.Request.UserAgent(),
+	//		param.ErrorMessage,
+	//	)
+	//}))
+	//r.Use(gin.Recovery())
+}
+
+// Booking contains binded and validated data.
+type Booking struct {
+	CheckIn  time.Time `form:"check_in" binding:"required,bookabledate" time_format:"2006-01-02"`
+	CheckOut time.Time `form:"check_out" binding:"required,gtfield=CheckIn" time_format:"2006-01-02"`
+}
+
+func bookableDate(
+	v *validator.Validate, topStruct reflect.Value, currentStructOrField reflect.Value,
+	field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string,
+) bool {
+	if date, ok := field.Interface().(time.Time); ok {
+		today := time.Now()
+		if today.Year() > date.Year() || today.YearDay() > date.YearDay() {
+			return false
+		}
+	}
+	return true
+}
+
+func getBookable(c *gin.Context) {
+	var b Booking
+	if err := c.ShouldBindWith(&b, binding.Query); err == nil {
+		c.JSON(http.StatusOK, gin.H{"message": "Booking dates are valid!"})
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+}
+
+func setVdalidation() {
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("bookabledate", bookableDate)
+	}
+}
+
 func setupRouter() *gin.Engine {
+	// Force log's color
+	gin.ForceConsoleColor()
 	// Disable Console Color
 	// gin.DisableConsoleColor()
 	r := gin.Default()
+	//r.Use(Logger())
+	// same as
+	// config := cors.DefaultConfig()
+	// config.AllowAllOrigins = true
+	// router.Use(cors.New(config))
+	r.Use(cors.Default())
 
+	setR(r)
+
+	//settings of validation
+	setVdalidation()
+
+	r.GET("/bookable", getBookable)
 	// Ping test
 	r.GET("/ping", func(c *gin.Context) {
+		cCp := c.Copy()
+		go func() {
+			// simulate a long task with time.Sleep(). 5 seconds
+			time.Sleep(2 * time.Second)
+
+			// note that you are using the copied context "cCp", IMPORTANT
+			log.Println("Done! in path " + cCp.Request.URL.Path)
+		}()
 		c.JSON(http.StatusOK, gin.H{"response": "好！", "code": http.StatusOK})
 	})
+
+	// Ping test
+	//r.GET("/logging", func(c *gin.Context) {
+	//	example := c.MustGet("www.baidu.com").(string)
+	//
+	//	// it would print: "192.168.1.229"
+	//	log.Println(example)
+	//	c.JSON(http.StatusOK, gin.H{"response": "好！", "code": http.StatusOK})
+	//})
 
 	// Get user value
 	r.GET("/user/:name", func(c *gin.Context) {
@@ -104,7 +216,42 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
-	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8008")
+	router := setupRouter()
+	// Listen and Server in 0.0.0.0:8008
+	srv := &http.Server{
+		Addr:           ":8008",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscanll.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	// catching ctx.Done(). timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server exiting")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/elitecodegroovy/gnetwork
 
 go 1.12
 
-require github.com/gin-gonic/gin v1.4.0
+require (
+	github.com/gin-contrib/cors v1.3.0
+	github.com/gin-gonic/gin v1.4.0
+	gopkg.in/go-playground/validator.v8 v8.18.2
+)
