89c1224349969f4d6f0e75ab3b1958a71680f4e6
elitecodegroovy <elite_jigang@163.com>
2020-01-09 10:08:41 +0800

    removed the useless

--- apps/pattern/00-singleton/singleton_test.go
+++ apps/pattern/00-singleton/singleton_test.go
@@ -8,11 +8,9 @@ import (
 //单例模式测试代码
 func TestCounter(t *testing.T) {
 	counter1 := GetInstance()
-
 	assert.EqualValues(t, 1, counter1.AddOne())
 	println("count 1 : ", counter1.count)
 
 	counter2 := GetInstance()
-
 	assert.EqualValues(t, 2, counter2.AddOne())
 }
