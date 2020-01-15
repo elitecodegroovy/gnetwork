1e1983b7baf427f6071db388544868f2f074b72f
elitecodegroovy <elite_jigang@163.com>
2020-01-15 23:40:18 +0800

    updated

--- apps/pattern/00-singleton/singleton.go
+++ apps/pattern/00-singleton/singleton.go
@@ -9,13 +9,25 @@ type Counter struct {
 }
 
 var instance *Counter
+var mu sync.Mutex
+var once sync.Once
 
 func GetInstance() *Counter {
 	if instance == nil {
+		mu.Lock()
+		defer mu.Unlock()
 		instance = new(Counter)
 	}
 	return instance
 }
+
+func GetInstanceByOnce() *Counter {
+	once.Do(func() {
+		instance = new(Counter)
+	})
+	return instance
+}
+
 func (s *Counter) AddOne() int {
 	s.Lock()
 	defer s.Unlock()
